ebaabc1e2937f052be448ca43d001a85ddda8bc2
TongyuYue <mycraftmw@163.com>
2016-11-03 02:24:00 +0800

    remove laji

---
+++ Phylab_src/resources/views/errors/phylab.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <!--<h1>error: {{$message}}, code: {{$code}}, httpCode: {{$httpCode}}</h1>-->
+<script src="/js/global.js"></script>
+<script>//error();</script>
+</body>
+</html>
