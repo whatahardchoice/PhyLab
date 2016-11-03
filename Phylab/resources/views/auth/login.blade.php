31a24d5856400e74c94ca8cb28039c2ff22729b3
dengchuyun <dengchuyun@hotmial.com>
2016-11-03 23:38:42 +0800

    Revert "登录状态修改"
    This reverts commit 684140a1eadbf7e4a129753fd2f178d81842ebdf.

--- Phylab/resources/views/auth/login.blade.php
+++ Phylab/resources/views/auth/login.blade.php
@@ -71,7 +71,7 @@
             <div class="alert alert-danger text-center" role="alert" id="login-alert">
               <i class="fa fa-remove"></i><span id="alert-message">用户名或密码错误</span>
             </div>
-            <form role="form" method="POST" action="{{URL::route('login')}}">
+            <form role="form" method="POST">
               <div class="form-group login">
                 <div class="input-group">
                   <div class="input-group-addon">
