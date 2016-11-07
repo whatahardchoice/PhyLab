4496bb174cb53d95580b22e251076dbb09381e9f
Yic <yicotn@qq.com>
2016-11-07 22:11:00 +0800

    fix handler

--- Phylab/storage/app/script/handler.py
+++ Phylab/storage/app/script/handler.py
@@ -5,15 +5,17 @@ import sys
 import traceback
 import os
 import platform
+import xml.dom.minidom
 
 available_lab = ['1010113', '1010212', '1070212', '1070312', '1070322', '1080114', '1080124', '1090114']
+xmlid = {'1010113':'', '1010212':'', '1070212':'10711', '1070312':'10712', '1070322':'', '1080114':'10811', '1080124':'', '1090114':''}
 handledir = '/var/www/buaaphylab/storage/app/script/' if platform.system() == 'Linux' else 'D:/Apache24/htdocs/PhyLab/Phylab/storage/app/script/'
 texdir = handledir + 'tex/'
 sys.path.append(handledir)
 
 if __name__ == '__main__':
 	# 三个命令行参数
-	# 第一个argv[1]是实验编号，如101031
+	# 第一个argv[1]是实验编号，如1010113
 	# 第二个argv[2]是XML文件所在地址，采用绝对路径
 	# 第三个argv[3]是最终生成的pdf文件的目标地址，采用绝对路径
 
@@ -30,7 +32,19 @@ if __name__ == '__main__':
 			if (lab == sys.argv[1]):
 				#from p1010113 import handler
 				#eval('from p' + lab + ' import handler')
-				latex_body = __import__('p'+lab).handler(handledir + 'test/' + lab + 'test/' + lab + '.xml')#(sys.argv[2])
+				testxml = handledir + 'test/' + lab + 'test/' + lab + '.xml'
+				try:
+					sublab = ''
+					dom = xml.dom.minidom.parse(testxml)
+					root = dom.documentElement
+					sublab_list = root.getElementsByTagName('sublab')
+					for l in sublab_list:
+						if (l.getAttribute("status") == 'true') & (l.getAttribute("id") == xmlid[lab]):
+							sublab = l
+				except Exception as e:
+					# raise e
+					pass
+				latex_body = __import__('p'+lab).handler(sublab)#(sys.argv[2])
 				flag = False
 				break
 		if (flag):
