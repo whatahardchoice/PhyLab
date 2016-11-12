e69282fb562b1916da0ee745796d4034b9c097e7
dengchuyun <dengchuyun@hotmial.com>
2016-11-12 23:17:26 +0800

    显示预习报告

--- Phylab/resources/views/report/index.blade.php
+++ Phylab/resources/views/report/index.blade.php
@@ -65,33 +65,169 @@
             <span style="color: white">物理实验<small>数据报告中心</small></span>
             <hr style="border-color: #08c093;border-width: 2px;margin: 0px">
           </h1>
-          <div class="btn-group labdoc-btns col-xs-6 col-md-6">
+          <div class="btn-group labdoc-btns col-xs-12" id="lab-group-select">
             <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" style="width: 100%;margin-bottom: 1px;">
               实验分组 <span class="caret"></span>
             </button>
             <ul class="dropdown-menu" role="menu">
-              <li><a href="#">1011</a></li>
+              <li id="lab-group-1011"><a href="#">1011 力学系列实验</a></li>
+              <li id="lab-group-1021"><a href="#">1021 热学系列实验</a></li>
+              <li id="lab-group-1031"><a href="#">1031 示波器的应用</a></li>
+              <li id="lab-group-1061"><a href="#">1061 薄透镜和单球面镜焦距的测量</a></li>
+              <li id="lab-group-1071"><a href="#">1071 分光仪的调整及其应用</a></li>
+              <li id="lab-group-1081"><a href="#">1081 光的干涉实验I（分波阵面法）1</a></li>
+              <li id="lab-group-1082"><a href="#">1082 光的干涉实验I（分波阵面法）2</a></li>
+              <li id="lab-group-1091"><a href="#">1091 光的干涉实验II（分振幅法）</a></li>
             </ul>
           </div>
-          <div class="btn-group labdoc-btns col-xs-6 col-md-6">
+          <div class="btn-group labdoc-btns col-xs-12"  id="lab-select">
             <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" style="width: 100%;margin-top: 1px">
               题目编号 <span class="caret"></span>
             </button>
             <ul class="dropdown-menu" role="menu">
-              <li><a href="#">1010113</a></li>
+              <li id="default-lab-name"><a href="#">未选择实验分组</a></li>
+              <div class="lab-name-list" id="lab-1011-list">
+                <li id="lab-1010113"><a href="#">1010113 拉伸法测钢丝弹性模量</a></li>
+                <li id="lab-1010212"><a href="#">1010212 扭摆法测量转动惯量</a></li>
+              </div>
+              <div class="lab-name-list" id="lab-1021-list">
+                <li id="lab-1020113"><a href="#">1020113 测量冰的溶解热实验</a></li>
+                <li id="lab-1020212"><a href="#">1020212 电热法测量焦耳热功当量实验</a></li>
+              </div>
+              <div class="lab-name-list" id="lab-1031-list">
+                <li id="lab-1030113"><a href="#">1030113 模拟示波器的使用（必做）</a></li>
+              </div>
+              <div class="lab-name-list" id="lab-1061-list">
+                <li id="lab-1060111"><a href="#">1060111 物距像距法测量透镜焦距</a></li>
+                <li id="lab-1060213"><a href="#">1060213 自准直法测量透镜焦距</a></li>
+                <li id="lab-1060312"><a href="#">1060312 共轭法测量凸透镜焦距</a></li>
+                <li id="lab-1060412"><a href="#">1060412 单球面镜焦距的测量</a></li>
+              </div>
+              <div class="lab-name-list" id="lab-1071-list">
+                <li id="lab-1070212"><a href="#">1070212 测量三棱镜的顶角（必选）</a></li>
+                <li id="lab-1070312"><a href="#">1070312 最小偏向角法测量棱镜的折射率</a></li>
+              </div>
+              <div class="lab-name-list" id="lab-1081-list">
+                <li id="lab-1080114"><a href="#">1080114 激光双棱镜干涉</a></li>
+              </div>
+              <div class="lab-name-list" id="lab-1082-list">
+                <li id="lab-1080215"><a href="#">1080215 钠光双棱镜干涉</a></li>
+                <li id="lab-1080225"><a href="#">1080225 钠光劳埃镜干涉</a></li>
+              </div>
+              <div class="lab-name-list" id="lab-1091-list">
+                <li id="lab-1090114"><a href="#">1090114 钠光劳埃镜干涉</a></li>
+                <li id="lab-1090212"><a href="#">1090212 牛顿环干涉</a></li>
+                <li id="lab-1090312"><a href="#">1090312 劈尖干涉</a></li>
+              </div>
             </ul>
           </div>
         </div>
       </div>
-      <div class="container tab-content" style="border-bottom-left-radius: 2px;border-bottom-right-radius: 2px;margin-bottom: 2px;">
+      <div class="container tab-content" style="border-bottom-left-radius: 2px;border-bottom-right-radius: 2px;margin-bottom: 2px;min-height: 500px;">
         <div class="tab-pane active " role="tabpanel" id="labdoc" style="overflow-x: auto;overflow-y: auto;max-height: 704px;text-align: center;">
-          <h1>未选择子实验</h1>
+          <div role="tabpanel" class="tab-pane fade in active" id="lab_1011_1">
+            <table class="table table-condensed table-hover table-striped" id="table1011_1_Lhb">
+              <thead>
+              <tr>
+                <th><label for="1011_1_gangsi">钢丝长L(cm)</label></th>
+                <th><label for="1011_1_pingmianjing">平面镜到标尺H(cm)</label></th>
+                <th><label for="1011_1_guangganggan">光杠杆前后足b(cm)</label></th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td><input class="para 1011_1 form-control" aria-label="钢丝长L" id="1011_1_gangsi" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="平面镜到标尺H" id="1011_1_pingmianjing" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="光杠杆前后足b" id="1011_1_guangganggan" type="text"></td>
+              </tr>
+              </tbody>
+            </table>
+            <table class="table table-condensed table-hover table-striped" id="table1011_1_zhijing">
+              <thead>
+              <tr>
+                <th>直径 </th>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+                <th>4</th>
+                <th>5</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>(mm)</td>
+                <td><input class="para 1011_1 form-control" aria-label="直径1" id="1011_1_D1" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="直径2" id="1011_1_D2" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="直径3" id="1011_1_D3" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="直径4" id="1011_1_D4" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="直径5" id="1011_1_D5" type="text"></td>
+              </tr>
+              </tbody>
+            </table>
+            <table class="table table-condensed table-hover table-striped" id="table1011_1_jiali">
+              <thead>
+              <tr>
+                <th>加力(kg)</th>
+                <td>读数(加力)</td>
+                <td>读数(减力)</td>
+
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>10</td>
+                <td><input class="para 1011_1 form-control" aria-label="加力10" id="1011_1_jia10" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="减力10" id="1011_1_jian10" type="text"></td>
+              </tr>
+              <tr>
+                <td>12</td>
+                <td><input class="para 1011_1 form-control" aria-label="加力12" id="1011_1_jia12" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="减力12" id="1011_1_jian12" type="text"></td>
+              </tr>
+              <tr>
+                <td>14</td>
+                <td><input class="para 1011_1 form-control" aria-label="加力14" id="1011_1_jia14" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="减力14" id="1011_1_jian14" type="text"></td>
+              </tr>
+              <tr>
+                <td>16</td>
+                <td><input class="para 1011_1 form-control" aria-label="加力16" id="1011_1_jia16" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="减力16" id="1011_1_jian16" type="text"></td>
+              </tr>
+              <tr>
+                <td>18</td>
+                <td><input class="para 1011_1 form-control" aria-label="加力18" id="1011_1_jia18" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="减力18" id="1011_1_jian18" type="text"></td>
+              </tr>
+              <tr>
+                <td>20</td>
+                <td><input class="para 1011_1 form-control" aria-label="加力20" id="1011_1_jia20" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="减力20" id="1011_1_jian20" type="text"></td>
+              </tr>
+              <tr>
+                <td>22</td>
+                <td><input class="para 1011_1 form-control" aria-label="加力22" id="1011_1_jia22" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="减力22" id="1011_1_jian22" type="text"></td>
+              </tr>
+              <tr>
+                <td>24</td>
+                <td><input class="para 1011_1 form-control" aria-label="加力24" id="1011_1_jia24" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="减力24" id="1011_1_jian24" type="text"></td>
+              </tr>
+              <tr>
+                <td>26</td>
+                <td><input class="para 1011_1 form-control" aria-label="加力26" id="1011_1_jia26" type="text"></td>
+                <td><input class="para 1011_1 form-control" aria-label="减力26" id="1011_1_jian26" type="text"></td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
         <div class="labdoc-btns">
           <button class="btn btn-success" style="margin-bottom: 5px;">
             <i class="fa fa-file-pdf-o"></i>查看预习报告
           </button>
-          <button class="btn btn-success" style="margin-bottom: 5px;">
+          <button id="button-generate-report" class="btn btn-success" style="margin-bottom: 5px;">
             <i class="fa fa-area-chart"></i>生成数据报告
           </button>
         </div>
@@ -104,7 +240,7 @@
         <div class="panel-heading"  style="background-color: #2b2f36;border: solid;border-width: 2px;border-color: black;">
           <div class="panel-title">
             <h4 class="panel-title text-center">
-              <span><span id="lab-status" class="badge">实验报告&nbsp; <span id="lab-text" title="false">1011</span>&nbsp; 数据</span></span>
+              <span id="lab-status" class="badge">物理实验选择策略</span>
             </h4>
           </div>
         </div>
@@ -130,13 +266,13 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="./js/bootstrap.min.js"></script>
-<script src="./js/global.js"></script>
-<script src="./js/reportCore.js"></script>
-<script src="./js/pdfobject.js"></script>
-<script src="./js/xmlInteraction.js"></script>
-<script src="./js/star.js"></script>
-<script src="./js/test.js"></script>
-<script src="./js/statistics.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/global.js"></script>
+<script src="../js/reportCore.js"></script>
+<script src="../js/pdfobject.js"></script>
+<script src="../js/xmlInteraction.js"></script>
+<script src="../js/star.js"></script>
+<script src="../js/test.js"></script>
+<script src="../js/statistics.js"></script>
 </body>
 </html>
