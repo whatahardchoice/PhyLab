c83364d400c9e4352340e3c7d19bfc8ad8351569
dengchuyun <dengchuyun@hotmial.com>
2016-11-14 02:03:39 +0800

    修改测设pdf路径

--- Phylab/public/js/reportCore.js
+++ Phylab/public/js/reportCore.js
@@ -231,12 +231,7 @@ var labDoc3dot1415926;
     });
   })
   $('#button-generate-report').click(function () {
-    var jqxhr = $.ajax('./zichen', {
-    	data: 'id=1010113'
-			}).done(function (data) {
-				$("#pdf_object").attr("data",data);
-    		$('#pdf_embed').attr("src",data);
-			};
+
   })
 
 	function changePdf(type,pdfName){
