453d492749146a54b8716c7a6fe05fcccf73de25
Yic <yicotn@qq.com>
2016-11-14 20:58:34 +0800

    change tmp_pdf to /tmp

--- Phylab/app/Http/Controllers/DebugScriptController.php
+++ Phylab/app/Http/Controllers/DebugScriptController.php
@@ -8,7 +8,7 @@ class DebugScriptController extends Controller {
         echo 'debug';
         $id = $_GET['id'];
         $xml = '/var/www/buaaphylab/storage/app/script/test/'.$id.'test/'.$id.'.xml';
-        $pdf = "/root/tmp_pdf/".$id;
+        $pdf = "/tmp/tmp_pdf/".$id;
         $res = exec("python /var/www/buaaphylab/storage/app/script/handler.py ".$id.' '.$xml." $pdf 2>&1 ", $output,$rval);
         if($rval!=0) {
             foreach ($output as $i => $o) {
