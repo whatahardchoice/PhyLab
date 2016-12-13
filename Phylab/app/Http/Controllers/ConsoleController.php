6da9098ceb90a5aa878c6f0149a7073f73095679
WeitaoFeng <fwtt20071028@126.com>
2016-12-14 01:20:05 +0800

    admin

--- Phylab/app/Http/Controllers/ConsoleController.php
+++ Phylab/app/Http/Controllers/ConsoleController.php
@@ -9,6 +9,12 @@ class ConsoleController extends Controller {
 
 	public function index() {
 		$exists=Auth::check()&&((Console::where('email','=',Auth::user()->email)->get()->count())>0);
+		if ($exists) {
+			return Auth::user()->email;
+		}
+		else {
+			return "false";
+		}
 		$isAdmin=$exists;
 		if (!$isAdmin) {
 			header("Location:".URL::to('/'));
