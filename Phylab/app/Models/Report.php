8421407f1520b0317c3ff92c664adfef7056cf4b
TongyuYue <mycraftmw@163.com>
2016-12-14 02:37:34 +0800

    重置user seed

--- Phylab/app/Models/Report.php
+++ Phylab/app/Models/Report.php
@@ -8,7 +8,7 @@ class Report extends Model
 {
     //
     protected $table = 'reports';
-    protected $fillable = ['experiment_id','script_link ','experiment_name','prepare_link','experiment_tag'];
+    protected $fillable = ['experiment_id','experiment_name','prepare_link','experiment_tag'];
     public function stars(){
         return $this->hasMany('App\Models\Star','report_id','id');
     }
