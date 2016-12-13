a1d7d62abc61f1cb43a960d0fa428e02206ef877
WeitaoFeng <fwtt20071028@126.com>
2016-12-13 22:16:19 +0800

    admin

--- Phylab/app/Http/Controllers/ConsoleController.php
+++ Phylab/app/Http/Controllers/ConsoleController.php
@@ -8,29 +8,30 @@ use Auth;
 class ConsoleController extends Controller {
 
 	public function index() {
-		$exists=Console::where('email','=',Auth::user()->email)->count();
-		$isAdmin=$exists>0;
-		if (!$isAdmin) {
-			header("Location:".URL::to('/'));
-		}
-		$ad=Console::where('email','=',Auth::user()->email)->first();
-		$st=$ad->status;
-        $data = ["reportTemplates"=>[],
-                 "username"=>Auth::user()->name,
-				 "auth"=>$isAdmin,
-				 "status"=>$st
-				 ];
-        $reports = Report::orderBy('experiment_id')->get();
-        foreach ($reports as $report) {
-            $rearr = array(
-                "id"=>$report->id,
-                "experimentId"=>$report->experiment_id,
-                "experimentName"=>$report->experiment_name,
-                "prepareLink"=>$report->prepare_link
-                );
-            array_push($data["reportTemplates"],$rearr);
-        }
-        return view("console.index",$data);
+		return "admin";
+		// $exists=Console::where('email','=',Auth::user()->email)->count();
+		// $isAdmin=$exists>0;
+		// if (!$isAdmin) {
+			// header("Location:".URL::to('/'));
+		// }
+		// $ad=Console::where('email','=',Auth::user()->email)->first();
+		// $st=$ad->status;
+        // $data = ["reportTemplates"=>[],
+                 // "username"=>Auth::user()->name,
+				 // "auth"=>$isAdmin,
+				 // "status"=>$st
+				 // ];
+        // $reports = Report::orderBy('experiment_id')->get();
+        // foreach ($reports as $report) {
+            // $rearr = array(
+                // "id"=>$report->id,
+                // "experimentId"=>$report->experiment_id,
+                // "experimentName"=>$report->experiment_name,
+                // "prepareLink"=>$report->prepare_link
+                // );
+            // array_push($data["reportTemplates"],$rearr);
+        // }
+        // return view("console.index",$data);
 	}
 
     public function getTable()
