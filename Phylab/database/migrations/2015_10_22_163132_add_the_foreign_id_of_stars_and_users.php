2b8a75d2ae8c66b35dd18812f28262aebb5d230e
TongyuYue <mycraftmw@163.com>
2016-12-14 15:50:00 +0800

    测试

--- Phylab/database/migrations/2015_10_22_163132_add_the_foreign_id_of_stars_and_users.php
+++ Phylab/database/migrations/2015_10_22_163132_add_the_foreign_id_of_stars_and_users.php
@@ -15,7 +15,6 @@ class AddTheForeignIdOfStarsAndUsers extends Migration
         //
         Schema::table('stars', function($table){
             $table->integer('user_id')->unsigned()->index();
-            
             $table->integer('report_id')->unsigned()->index();
             $table->foreign('report_id')->references('id')->on('reports')->onDelete('restrict')->onUpdate('restrict');
         });
@@ -30,7 +29,6 @@ class AddTheForeignIdOfStarsAndUsers extends Migration
     {
         //
         Schema::table('stars', function(Blueprint $table) {
-            
             $table->dropForeign('stars_report_id_foreign');
             $table->dropColumn('user_id');
             $table->dropColumn('report_id');
