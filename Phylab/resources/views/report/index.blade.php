be374c89685939918b7f4e16a70a254f0d671e5b
dengchuyun <dengchuyun@hotmial.com>
2016-12-14 21:54:03 +0800

    测试评论添加接口

--- Phylab/resources/views/report/index.blade.php
+++ Phylab/resources/views/report/index.blade.php
@@ -264,8 +264,9 @@
         $(document).ready(function(){
             $('#report-num').text($('#collection-iframe').contents().find('#collection-list').children().length);
         });
-        $.get(G_BASE_URL + '/wecenter/?/article/main/index', {
-            'id': 7,
+        $.post(G_BASE_URL + '/wecenter/?/article/ajax/save_comment/', {
+            'article_id': 7,
+            'message': 'test'
         }).done(function (data) {
             alert('成功, 收到的数据: ' + JSON.parse(data)['err']);
         }).fail(function (xhr, status) {
