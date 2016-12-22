c4e3a46f03280612b85db0262d922e7220266a0d
TongyuYue <mycraftmw@163.com>
2016-12-23 06:45:11 +0800

    修正report的index

--- Phylab/app/Http/Controllers/ReportController.php
+++ Phylab/app/Http/Controllers/ReportController.php
@@ -27,8 +27,11 @@ class ReportController extends Controller
         $data = ['reports'=>array(),
                 'username'=>Auth::user()->name,
                 'auth'=>true];
+        $exists=Auth::check()&&((Console::where('email','=',Auth::user()->email)->get()->count())>0);
+		$isAdmin=$exists;
         $reports = Report::orderBy('experiment_tag')->get();
         foreach ($reports as $report) {
+            if (!$isAdmin&&($report->status&1)==0) continue;
             $rearr = array(
                 "id"=>$report->experiment_id,
                 "experimentName"=>$report->experiment_name,
