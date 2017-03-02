975af30c61ef1b0715de7ae485099e0e15af24bd
TongyuYue <mycraftmw@163.com>
2017-03-03 01:46:19 +0800

    添加DB模块

--- Phylab/app/Http/Controllers/ReportController.php
+++ Phylab/app/Http/Controllers/ReportController.php
@@ -13,6 +13,7 @@ use App\Exceptions\App\FileIOException;
 use App\Exceptions\App\NoResourceException;
 use Exception;
 use Config;
+use DB;
 class ReportController extends Controller
 {
     /**
@@ -323,25 +324,25 @@ class ReportController extends Controller
         }
         $report = Report::where('experiment_id','=',Request::get('reportId'))->first();
         if($report){
-            $report->status = 1;
-            $results = DB::select('select * from wc_category where title = ?', [$report->experiment_tag]);
-			if ($results->count() == 0) {
-                DB::insert('insert into wc_category (title, type, parent_id, sort) values (?, ?, ?, ?)', [$report->experiment_tag, 'question', 1, 0]);
-			}
-			// $results = DB::select('select * from wc_category where title = ?', [$report->experiment_tag]);
-			// $category_id = $results->first()->id;
-            // $time = time();
-			// DB::insert('insert into wc_article (uid, title, message, comments, views, add_time, has_attach, lock, votes, title_fulltext, category_id, is_recommend, sort) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [26, $lab_id.'总评论贴',$lab_id.'总评论贴',0,0,$time,0,0,0,$report->experiment_id." 3578035770",$category_id,0,0]);
-            // $report->related_article = DB::select('select * from wc_article where uid = 26 and add_time = ?', [$time])->first()->id;
-
-            $report->save();
+            DB::transaction(function () {
+                $report->status = 1;
+                $results = DB::select('select * from wc_category where title = ?', [$report->experiment_tag]);
+                if ($results->count() == 0) {
+                    DB::insert('insert into wc_category (title, type, parent_id, sort) values (?, ?, ?, ?)', [$report->experiment_tag, 'question', 1, 0]);
+                }
+                $results = DB::select('select * from wc_category where title = ?', [$report->experiment_tag]);
+                $category_id = $results->first()->id;
+                $time = time();
+                DB::insert('insert into wc_article (uid, title, message, comments, views, add_time, has_attach, lock, votes, title_fulltext, category_id, is_recommend, sort) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [26, $lab_id.'总评论贴',$lab_id.'总评论贴',0,0,$time,0,0,0,$report->experiment_id." 3578035770",$category_id,0,0]);
+                $report->related_article = DB::select('select * from wc_article where uid = 26 and add_time = ?', [$time])->first()->id;
+                $report->save();
+            });
             $data['status'] = SUCCESS_MESSAGE;
             $data['message'] = "发布成功";
         }
         else{
             $data['status'] = FAIL_MESSAGE;
             $data['message'] = "发布失败";
-            exec("touch ".Config::get('phylab.experimentViewPath')."haveBug.html",$output,$reval1);
         }
         return response()->json($data);
         //return view("report.show",$data);
