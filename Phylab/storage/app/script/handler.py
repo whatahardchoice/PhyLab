778e55d21c1a5da9a0a758de51b91c248dad3452
lee <leext@buaa.edu.cn>
2017-11-10 17:18:40 +0800

    修改了latex脚本处理部分，使得可以正常运行

--- Phylab/storage/app/script/handler.py
+++ Phylab/storage/app/script/handler.py
@@ -8,21 +8,22 @@ import os
 import platform
 import xml.dom.minidom
 
-scriptdir = '/var/www/buaaphylab/storage/app/script/' if platform.system() == 'Linux' else 'C:/Users/CFREE/Documents/Github/auto-deploy-phylab/Phylab/storage/app/script/'
+scriptdir = '/var/www/Phylab/storage/app/script/' if platform.system(
+) == 'Linux' else 'C:/Users/CFREE/Documents/Github/auto-deploy-phylab/Phylab/storage/app/script/'
 texdir = scriptdir + 'tex/'
 sys.path.append(scriptdir)
 
+print 'you know'
 if __name__ == '__main__':
     # 三个命令行参数
     # 第一个argv[1]是实验编号，如1010113
     # 第二个argv[2]是XML文件所在地址，采用绝对路径
     # 第三个argv[3]是最终生成的pdf文件的目标地址，采用绝对路径
 
-
     try:
         latex_head_file = open(texdir + 'Head.tex', 'r')
         latex_head = latex_head_file.read().decode('utf-8', 'ignore')
-        latex_head_file.close();
+        latex_head_file.close()
         latex_tail = "\n\\end{document}"
         latex_body = ""
 
@@ -33,7 +34,8 @@ if __name__ == '__main__':
         except Exception as e:
             # raise e
             pass
-        latex_body = __import__('p' + sys.argv[1]).handler(root)  # (sys.argv[2])
+        latex_body = __import__(
+            'p' + sys.argv[1]).handler(root)  # (sys.argv[2])
         if (not os.path.isfile(scriptdir + 'p' + sys.argv[1] + '.py')):
             print('{"status":"fail", "msg":"no handler"}')
             exit(1)
@@ -45,9 +47,10 @@ if __name__ == '__main__':
 
         os.chdir(os.path.dirname(sys.argv[3]))
         # 等于１时是错误
-        ret = subprocess.call("pdflatex -interaction=nonstopmode " + sys.argv[3] + ".tex", shell=True)
+        ret = subprocess.call(
+            "xelatex -interaction=nonstopmode " + sys.argv[3] + ".tex", shell=True)
         subprocess.call("rm " + sys.argv[3] + ".aux", shell=True)
-        subprocess.call("rm " + sys.argv[3] + ".synctex*", shell=True)
+        #subprocess.call("rm " + sys.argv[3] + ".synctex*", shell=True)
         subprocess.call("rm " + sys.argv[3] + ".log", shell=True)
 
         if ret == 0:
