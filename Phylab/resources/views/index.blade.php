54de4909e3c445d01f8eca47e850dd1eb86de3aa
wangjianbing <3502381452@qq.com>
2019-05-13 17:56:21 +0800

    Merge branch 'dev' of github.com:whatahardchoice/PhyLab into dev
    # Conflicts:
    #	Phylab/resources/views/layout/main.blade.php
    #	Phylab/resources/views/report/index.blade.php
    #	Phylab/resources/views/user/index.blade.php

--- Phylab/resources/views/index.blade.php
+++ Phylab/resources/views/index.blade.php
@@ -46,7 +46,7 @@
           </div>
             </div>
         </div>
-      <div  class="col-xs-12 col-md-6 col-md-offset-6" style="margin-top: 5px">
+      <div  class="col-xs-12 col-md-6 col-md-offset-6"  @if ($auth) style="display: block; margin-top: 5px" @else style="display: none" @endif>
         <label class="col-md-12 col-xs-12 text-center" style="background-color: #aeb3b9" ><strong style="font-size: 20px">公告</strong>
           <marquee style=" HEIGHT: 50px; background-color: #aeb3b9 " scrollamount="1" direction="up" Align="Middle" >
             <div style="text-align: center;">
