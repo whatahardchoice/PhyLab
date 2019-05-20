4d0d1ed9d47b1e2b51323a5c9da82b0f5e49acf7
ZBW <z@bwis.me>
2019-05-20 23:18:24 +0800

    修改设计性实验页面样式，增加提示

--- Phylab/resources/views/desexp/index.blade.php
+++ Phylab/resources/views/desexp/index.blade.php
@@ -5,12 +5,12 @@
   <!--  contents -->
   <div class="col-md-8 col-md-offset-2 col-xs-12 " style="margin-bottom: 50px">
 
-    <nav class="navbar navbar-default" style="background-color: #c2ccd0;margin-top: 25px;box-shadow:2px 2px 5px rgb(200,200,200);border:none;" role="navigation">
+    <nav class="navbar navbar-default" style="background-color: #08c093;margin-top: 25px;box-shadow:2px 2px 5px rgb(200,200,200);border:none;" role="navigation">
       <div class="container-fluid" style="display: flex; align-items: center">
         <div class="navbar-header col-xs-12  col-md-6 text-center">
-          <a class="navbar-brand" href="#"><strong style="font-size: large">设计性实验</strong></a>
+          <a class="navbar-brand" href="#"><strong style="font-size: large;color:white">设计性实验</strong></a>
         </div>
-        <text class="col-md-3">点击虚线显示/隐藏答案</text>
+        <text class="col-md-3" style="color:white">点击虚线显示/隐藏答案</text>
         <div class="text-center col-xs-12 col-md-3 col-md-offset-2">
             <select class="form-control" id="desexp-select" style="margin:0" onchange="switchContent()">
               <option selected="selected" disabled="disabled" style="display: none">请选择实验</option>
@@ -21,6 +21,12 @@
         </div>
       </div>
     </nav>
+    <div style="background: white; box-shadow: 2px 2px 3px rgb(200,200,200);margin-top:10px;margin-bottom:10px;padding:10px">
+      <div style="text-align: center">本模块内容均来自《北航基础物理实验考试资料2017修订版》</div>
+      <div style="text-align: center">感谢编写及修订此资料同学的帮助</div>
+      <div style="text-align: center">本站对此页面的内容<strong style="color:red">正确性不做保证</strong>，还请各位同学多多注意和指正</div>
+      <div style="text-align: center">祝各位同学取得好成绩！</div>
+    </div>
 
     <div style="margin-top: 0px;margin-bottom: 20px">
         <iframe id="desexp-iframe" src="/desexp_html/D01.html" onload="this.style.height=this.contentDocument.body.scrollHeight +'px';"
