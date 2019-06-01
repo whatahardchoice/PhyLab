4613b540bcf681c7be1de05ac32747fd0e9ae88e
fan_zikang <fan_zikang@163.com>
2019-06-01 19:50:15 +0800

    删除reportCore、global、consoleCore中的无用冗余代码，添加注释

--- Phylab/public/js/global.js
+++ Phylab/public/js/global.js
@@ -29,34 +29,7 @@
 			}
 		}
 	}
-	function SetDisable(index,if_disable){
-		var item = document.getElementById(index);
-		if(if_disable)
-			item.setAttribute("disabled","disabled");
-		else
-			item.removeAttribute("disabled")
-	}
-	
-	function setShowHide(show_index,hide_index,time_offset){
-		var show_item = document.getElementById(show_index);
-		var hide_item = document.getElementById(hide_index);
-		show_item.style.display = "block";
-		hide_item.style.display = "none";
-		if(time_offset<0)return;
-		else setTimeout(_setShowHide(hide_index,show_index),time_offset);
-	}
-	function _setShowHide(show_index,hide_index){
-		return function(){
-			setShowHide(show_index,hide_index,-1)
-		}
-	}
-	
-	function error(){
-		alert("你是不是做坏事了( >^<)");
-		alert("我讨厌你！TuT");
-		alert("滚吧！QAQ");
-		window.location = "http://www.taobao.com/"
-	}
+
 	function PostAjax(url,postData,cfunc){
 	    var xmlhttp;
 	        if (window.XMLHttpRequest)
--- Phylab/public/js/reportCore.js
+++ Phylab/public/js/reportCore.js
@@ -1,30 +1,9 @@
-
-var labDoc3dot1415926;
 var CUR_LAB_GROUP;
 var CUR_SUBLAB;
 var CUR_PDF;
 var CUR_COMMENT_GROUPS_NUM;
 var CUR_COMMENT_GROUPS_INDEX;
-function lab(index){
-    this.index = index;
-    this.dbId = getDbId(index);
-    this.xmlTxt;
-    this.flush = function(){
-        this.xmlTxt=SetXMLDoc_lab();
-    }
-    this.getIndex = function(){
-        return this.index;
-    }
-    this.getXML = function(){
-        if(this.xmlTxt!=null)return this.xmlTxt;
-    }
-    this.getDbId = function(){
-        return this.dbId;
-    }
-}
-function getDbId(index){
-    return $('#back_info a[index='+index+']').attr('db-id');
-}
+
 function check(){
     if(browser()=="FF"){
         document.getElementById('firefox_pdf').style.display='block';
@@ -45,184 +24,20 @@ function check(){
     $('#button-generate-report').attr("disabled", true);
     $('#collect-report').attr("disabled", true);
 }
-function eleDisable(){
-    SetDisable('importBtn',true);
-    SetDisable('collectBtn',true);
-    SetDisable('selectBtn',true);
-    SetDisable('exportBtn',true);
-    SetDisable('InputLabIndex',true);
-}
-function eleEnable(){
-    SetDisable('importBtn',false);
-    SetDisable('collectBtn',true);
-    SetDisable('exportBtn',true);
-}
-function eleReset(){
-    SetDisable('selectBtn',false);
-    SetDisable('importBtn',false);
-    SetDisable('InputLabIndex',false);
-}
-function collectEnable(){
-    SetDisable('collectBtn',false);
-    document.getElementById('collectIco').setAttribute("class","glyphicon glyphicon-star-empty");
-    document.getElementById('collectText').innerHTML = "收藏";
-}
-
-function collectLab(ico_id,txt_id){
-    var ico = document.getElementById(ico_id);
-    var txt = document.getElementById(txt_id);
-    var check = txt.innerHTML;
-    if(check=="取消收藏"){
-        deleteReportStar(ico,txt);
-    }
-    else if(check=="收藏"){
-        createStar(ico,txt);
-    }
-    else
-        alert("Button text can not be [txt] when use this function!Please Use 收藏/取消收藏");
-}
-function SelectLab(index,ref){
-    var lt = document.getElementById(ref);
-    if((new RegExp("^10(11|12|21|22|31|41|61|71|81|82|91)$")).test(index)){
-        labDoc3dot1415926 = new lab(index);
-        lt.innerHTML = '实验' + index + '分组预习报告';
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-//USE jquery version 2.1.4, bootstrap.min.js
-function inputCheck(){
-    var a = $.merge($("input.para"),$("input.var"));
-    for(var i = 0; i<a.length; i++) a[i].setAttribute("value",a[i].getAttribute("aria-label"));
-}
-function selectBtnClick(){
-    if(SelectLab($('#InputLabIndex')[0].value,'LabText')){
-        $('.alert').hide();
-        $('#LabStatus')[0].innerHTML = "预览";
-        changePdf('prepare',labDoc3dot1415926.getIndex()+".pdf",0);
-        eleEnable();
-    }
-    else{
-        $('.alert').show();
-    }
-}
-function importBtnClick(){
-    $("#lab_table_"+labDoc3dot1415926.getIndex()).modal("toggle");
-}
-function collectBtnClick(){
-    collectLab('collectIco','collectText');
-}
-function exportBtnClick(){
-    eleDisable();
-    try{
-        $('#lab_collapse').collapse('hide');
-        $('#loading-container').fadeIn();
-        setTimeout('Post_lab(errorFunction)',1000+Math.random()*2000);
-    }catch(e){
-        $('#loading-container').fadeOut();
-        error();
-    }
-}
 
 function errorFunction(message){
     alert(message);
 }
 
-$("#InputLabIndex").bind("keypress",function(){
-    if(event.keyCode==13) {
-        if(SelectLab($('#InputLabIndex')[0].value,'LabText')){
-            $('.alert').hide();
-            $('#LabStatus')[0].innerHTML = "预览";
-            changePdf('prepare',labDoc3dot1415926.getIndex()+".pdf",0);
-            eleEnable();
-            return false;
-        }
-        else $('.alert').show();
-        return false;
-    }
-    else return true;
-})
-$('a.lab_title').bind('click',function(){
-    //USE reportCore.js, bootstrap.min.js
-    if($('#InputLabIndex').attr("disabled")=="disabled")return;
-    if(SelectLab(this.title,'LabText')){
-        $('.alert').hide();
-        $('#LabStatus')[0].innerHTML = "预览";
-        changePdf('prepare',labDoc3dot1415926.getIndex()+".pdf",0);
-        eleEnable();
-    }
-    else $('.alert').show();
-});
-$('a.lab_index').bind('click',function(){
-    //USE reportCore.js, bootstrap.min.js
-    if($('#InputLabIndex').attr("disabled")=="disabled")return;
-    if(SelectLab(this.innerHTML,'LabText')){
-        $('.alert').hide();
-        $('#LabStatus')[0].innerHTML = "预览";
-        changePdf('prepare',labDoc3dot1415926.getIndex()+".pdf",0);
-        eleEnable();
-    }
-    else $('.alert').show();
-});
-$('input.para').bind('keyup',function(){
-    if((new RegExp("^\\d+(.\\d+)?$")).test(this.value)==false) $(this).addClass("wrong-input");
-    else $(this).removeClass("wrong-input")
-})
-$('input.var').bind('keyup',function(){
-    if((new RegExp("(^\\d+(.\\d+)?$)|(^$)")).test(this.value)==false) $(this).addClass("wrong-input");
-    else $(this).removeClass("wrong-input")
-})
-$('button.btn-Save').bind('click',function(){
-    var index = labDoc3dot1415926.getIndex();
-    var paraArray,varArray;
-    var labStr = "", wrong_count = 0, i = 1, ErrType = 1;
-    while((tp=document.getElementById("check_"+index+"_"+i))!=null){
-        if(tp.checked)labStr += "input.para"+"."+index+"_"+i+",";
-        i++;
-    }
-    //get selected sublab
-    if(labStr==""){
-        document.getElementById("ErrorText_"+index).innerHTML = "请先选择需要保存数据的子实验（￣▽￣）~*　)";
-        setShowHide("btnError_"+index,"btnSave_"+index,3000);
-    }
-    else{
-        labStr = labStr.substring(0,labStr.lastIndexOf(','));
-        paraArray = $(labStr);
-        labStr = labStr.replace(new RegExp("para","gm"),"var");
-        varArray = $(labStr);
-        //get data form input, para can't be null
-        paraArray.each(function(){
-            if($(this).hasClass("wrong-input")) wrong_count++;
-            else if(this.value==""){
-                wrong_count++;
-                $(this).addClass("wrong-input");
-            }
-            //else if((new RegExp("(^\\d+(.\\d+)?$)")).test(this.value)==false){error();return false;}
-        })
-        varArray.each(function(){
-            if($(this).hasClass("wrong-input")) wrong_count++;
-            //else if((new RegExp("(^\\d+(.\\d+)?$)|(^$)")).test(this.value)==false){error();return false;}
-        })
-        //check data
-        if(wrong_count==0){
-            $("#lab_table_"+index).modal('toggle');
-            if(labStr!=""){
-                SetDisable('exportBtn',false);
-                labDoc3dot1415926.flush();
-            }//when no selected sublab exist, just close modal
-        }
-        else{
-            document.getElementById("ErrorText_"+index).innerHTML = "有"+wrong_count+"处输入不合法，请检查标红输入框";
-            setShowHide("btnError_"+index,"btnSave_"+index,3000);
-        }
-    }
-})
-
+/**
+ * This function is used to change files to show(including pad and html)
+ *
+ * @param type
+ * @param fileName
+ * @param ishtml
+ */
 function changePdf(type,fileName,ishtml){
-    var path = ""
+    var path = "";
     if(type=="prepare"){
         path = "./prepare_pdf/";
     }
@@ -230,7 +45,7 @@ function changePdf(type,fileName,ishtml){
         path = "./pdf_tmp/";
     }
     else if(type=="star"){
-        path = "./star_pdf/"
+        path = "./star_pdf/";
     }
     if(ishtml){
         document.getElementById("show-html").style.display = 'block';
@@ -242,43 +57,13 @@ function changePdf(type,fileName,ishtml){
     $('#pdf_embed').attr("src",path+fileName);
     cp(path+fileName,ishtml);
 }
-function Post_lab(postErrorFunc){
-    var xmlString = labDoc3dot1415926.getXML();
-    var dbId = labDoc3dot1415926.getDbId();
-    var postData = "xml="+encodeURI(xmlString)+"&id="+dbId;
-    PostAjax("./report",postData,function(){
-        if (this.readyState==4 && this.status==200){
-            var jsonText = eval("(" + this.responseText + ")");
-            //alert(this.responseText);
-            //alert(jsonText["status"]);
-            if(jsonText["status"]=='success'){
-                changePdf('tmp',jsonText['link'],0);
-                $('#collectBtn').attr('link',jsonText['link']);
-                $('#loading-container').fadeOut();
-                eleReset();
-                $('#LabStatus')[0].innerHTML = "数据";
-                collectEnable();
-            }
-            else{
-                postErrorFunc(jsonText["message"]);
-                $('#loading-container').fadeOut();
-                eleReset();
-            }
-        }
-        else if(this.readyState==4 && this.status!=200){
-            postErrorFunc("生成报告失败");
-            $('#loading-container').fadeOut();
-            eleReset();
-        }
-    });
-}
 
 //PhyLab2.0新增脚本
+/**
+ * This function is to show report index
+ */
 function initReportPage() {
     check();
-    //$(document).ready(function () {
-    //  $('#report-num').text($($('#collection-iframe').contents().find('#collection-list')).attr('num'));
-    //});
     window.onload = function() {
       var num = document.getElementById('collection-iframe').contentWindow.document.getElementById('collection-list').getAttribute('data-value');
       $('#report-num').text(num);
@@ -287,9 +72,7 @@ function initReportPage() {
     $('#wait-report').css('width', $('#' + CUR_PDF).outerWidth());
     $('#reply-notice').css('height', $('#comment-editor').outerHeight());
     $('#reply-notice').css('width', $('#comment-editor').outerWidth());
-    // $.get('./report').done(function (data) {
-    //     data = JSON.parse(data);
-    // });
+
     $.get('./getreport').done(function (data) {
         for (var labgroup in data.reports) {
             $('#lab-list').append(
@@ -346,6 +129,9 @@ function initReportPage() {
     });
 }
 
+/**
+ * To cache your data
+ */
 function recordTableValue() {
     var inputs_val = localStorage.getItem($('#username').text() + CUR_SUBLAB + '-table');
     if (inputs_val) {
@@ -363,49 +149,9 @@ function recordTableValue() {
     }
 }
 
-// $('#preading-report').click(function () {
-//
-//     if (typeof CUR_SUBLAB === 'undefined')
-//     {
-//         $('#upload_preview_modal').modal('hide');
-//         alert("请先选择实验！");
-//         return false;
-//     }
-//
-//     let valid = checkInput();
-//     if (!valid)
-//         return false;
-//
-//     var xmlString = SetXMLDoc_lab();
-//     if (xmlString === null)
-//         return;
-//     var postData = 'id=' + CUR_SUBLAB + '&' + 'xml=' + xmlString;
-//
-//     PostAjax("./report",postData,function(){
-//         if (this.readyState==4 && this.status==200){
-//             var jsonText = eval("(" + this.responseText + ")");
-//             //alert(this.responseText);
-//             //alert(jsonText["status"]);
-//             if(jsonText["status"]=='success') {
-//                // window.open("pdf_tmp/"+jsonText['link'],  "_system");
-//                 var $form = $('<form method="GET" ></form>');
-//                 $form.attr('action', "pdf_tmp/"+jsonText['link']);
-//                 $form.appendTo($('body'));
-//                 $form.submit();
-//                 //  $('#lab-status').text('子实验' + CUR_SUBLAB + '数据报告');
-//                 $('#collect-report').attr('link',jsonText['link']);
-//                 $('#collect-report').removeAttr("disabled");
-//             }
-//             else
-//                 errorFunction(jsonText["message"]);
-//
-//         }
-//         else if(this.readyState==4 && this.status!=200) {
-//
-//             errorFunction("生成报告失败");
-//         }
-//     });
-// });
+/**
+ * To show preparation report
+ */
 $('#button-view-preparation').click(function () {
     changePdf('prepare',CUR_LAB_GROUP + ".pdf",0);
     $('#lab-status').text('实验组' + CUR_LAB_GROUP + '预习报告');
@@ -421,6 +167,9 @@ $('#button-generate-report').click(function () {
     }
 });
 
+/**
+ * To create latex report
+ */
 $('#button-generate-latex').click(function () {
 
     if (typeof CUR_SUBLAB === 'undefined')
@@ -442,8 +191,6 @@ $('#button-generate-latex').click(function () {
     PostAjax("./report/createTex",postData,function(){
         if (this.readyState==4 && this.status==200){
             var jsonText = eval("(" + this.responseText + ")");
-            //alert(this.responseText);
-            //alert(jsonText["status"]);
             if(jsonText["status"]=='success') {
 
                 changePdf('tmp',jsonText['link'],0);
@@ -466,6 +213,9 @@ $('#button-generate-latex').click(function () {
     $('#modal-report-select').modal('hide');
 });
 
+/**
+ * To create html report
+ */
 $('#button-generate-markdown').click(function () {
     if (typeof CUR_SUBLAB === 'undefined')
     {
@@ -486,8 +236,6 @@ $('#button-generate-markdown').click(function () {
     PostAjax("./report/createMD",postData,function(){
         if (this.readyState==4 && this.status==200){
             var jsonText = eval("(" + this.responseText + ")");
-            //alert(this.responseText);
-            //alert(jsonText["status"]);
             if(jsonText["status"]=='success') {
                 changePdf('tmp',jsonText['link'],1);
                 $('#preading-report').attr('disabled', false);
@@ -516,6 +264,10 @@ $('#collect-report').click(function () {
         deleteReportStar();
     }
 });
+
+/**
+ * Comment function
+ */
 
 $('#button-comment-reply').click(function () {
     sendComment(sessionStorage.getItem(CUR_SUBLAB + '_article_id'), $('#comment-editor').val());
